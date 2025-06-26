06e2f9785284da38f15c462d0394d6fa9d2db946
Kimimaybe29 <Kimishishengye@outlook.com>
2025-06-26 17:51:25 +0800

    Update sidebar.ts

--- docs/config/zh/sidebar.ts
+++ docs/config/zh/sidebar.ts
@@ -8,38 +8,43 @@ export const sidebar = {
                 ]
             },
             {
-                text: 'Kimi的万事屋',
-                items: [
-                    { text: '简介', link: '/docs/kimi/' },
-                    { text: '成员', link: '/docs/kimi/team' },
-                ]
-            },
-            {
-                text: '淡水之域',
-                items:[
-                    { text: '简介', link: '/docs/water/' },
-                    { text: '成员', link: '/docs/water/team' },
-                ]
-            },
-            {
-                text: '叶服',
-                items:[
-                    { text: '简介', link: '/docs/ye/' },
-                    { text: '成员', link: '/docs/ye/team' },
-                ]
-            },
-            {
-                text: '呦呦的后花园',
-                items:[
-                    { text: '简介', link: '/docs/yoyo/' },
-                    { text: '成员', link: '/docs/yoyo/team' },
-                ]
-            },
-            {
                 text:'游玩教程',
                 items:[
                     {text:'入服教学', link: '/docs/guide/'},
                     {text:'客户端下载', link: '/docs/guide/modpack'},
+                ]
+            },
+            {
+                text: '服务器' ,
+                items: [
+                    {
+                        text: 'Kimi的万事屋',
+                        items: [
+                            { text: '简介', link: '/docs/kimi/' },
+                            { text: '成员', link: '/docs/kimi/team' },
+                        ]
+                    },
+                    {
+                        text: '淡水之域',
+                        items:[
+                        { text: '简介', link: '/docs/water/' },
+                        { text: '成员', link: '/docs/water/team' },
+                        ]
+                    },
+                    {
+                        text: '叶服',
+                        items:[
+                            { text: '简介', link: '/docs/ye/' },
+                            { text: '成员', link: '/docs/ye/team' },
+                        ]
+                    },
+                    {
+                        text: '呦呦的后花园',
+                        items:[
+                            { text: '简介', link: '/docs/yoyo/' },
+                            { text: '成员', link: '/docs/yoyo/team' },
+                        ]
+                    }
                 ]
             }
         ]
