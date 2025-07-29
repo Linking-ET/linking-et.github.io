c1f5d9877530554c5e3a58cd0c637770f9e9e01b
XChen446 <3088576338@qq.com>
2025-07-30 05:34:24 +0800

    更换discord链接指向

--- docs/config/zh/CustomTexts.ts
+++ docs/config/zh/CustomTexts.ts
@@ -3,7 +3,7 @@ export const CustomTexts = {
     description: "欢迎加入LET万事屋群组服！",
     socialLinks: [
         { icon: 'github', link: 'https://github.com/linking-et/linking-et.github.io' },
-        { icon: 'discord', link: 'https://discord.gg/bmMJdAAJmQ' },
+        { icon: 'discord', link: 'https://discord.link-et.link' },
     ],
     docFooter: {
         prev: '上一页',
