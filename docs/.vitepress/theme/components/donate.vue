33abf319c02412851f36f60fc21a9b252c33aceb
LorienYang <yangluoying19277904322@outlook.com>
2025-09-21 23:34:40 +0800

    小修小补

--- docs/.vitepress/theme/components/donate.vue
+++ docs/.vitepress/theme/components/donate.vue
@@ -3,12 +3,14 @@
 </script>
 
 <template>
-<div flex="items-center justify-center" class="Main w-full max-w-574px min-h-200px flex
-            portrait:max-w-200px portrait:min-h-610px portrait:flex-col">
-  <div flex="items-center justify-center" class="IMG flex gap-5 flex-wrap ">
+<div flex="items-center justify-center"
+     portrait="max-w-200px min-h-610px flex-col"
+     class="Main w-full max-w-574px min-h-200px flex" >
+  <div flex="items-center justify-center"
+       class="IMG flex gap-5 flex-wrap">
     <img class="h-200px w-auto" src="/res/img/donate/Kimimaybe29.jpg" alt="Kimi">
-    <img class="h-200px w-auto" src="/res/img/donate/Lorienyang.jpg" alt="Kimi">
-    <img class="h-200px w-auto" src="/res/img/donate/freshwater.jpg" alt="Kimi">
+    <img class="h-200px w-auto" src="/res/img/donate/Lorienyang.jpg" alt="Lorien">
+    <img class="h-200px w-auto" src="/res/img/donate/freshwater.jpg" alt="Water">
   </div>
 </div>
 </template>
