b8f03a1e04f6b0b3796e24369eb7d9ce37a830bc
Kimimaybe29 <Kimishishengye@outlook.com>
2025-11-24 16:35:00 +0800

    英语页面稍后翻译，现在这样应该能合了 (#56)
    * 增加英文页脚
    
    * add(docs):增加英语页脚
    
    * Custom Texts, nav, sidebar翻译完毕
    
    引入文档
    
    ---------
    
    Co-authored-by: LorienYang <yangluoying19277904322@outlook.com>

--- docs/config/en/nav.ts
+++ docs/config/en/nav.ts
@@ -1,15 +1,44 @@
 import {DefaultTheme} from "vitepress";
 
 export const nav: DefaultTheme.NavItem[] = [
-    { text: 'main page', link: '/en' },
-    {text: 'about us',link: '/en/docs/info/about'},
-    {
-        text: 'server list',
+    { text: 'Home', link: '/en' },
+    { text: 'Friends',
+        items:[
+            { text: 'MSCPO', link: 'https://www.mscpo.top/' },
+            { text: 'SWA Network', link: 'https://www.swamc.top/' },
+            { text: 'EggMC', link: 'https://eggmc.top/' },
+            { text: 'KlpMC', link: 'https://klpmc.top/' },
+            { text: 'TOC', link: 'https://www.mminecraft.cn/' },
+        ]
+    },
+    { text: 'About',
         items: [
-            { text: 'Kimi home', link: '/en/docs/kimi' },
-            { text: 'freshwater region', link: '/en/docs/water' },
-            { text: 'leaf page',link: '/en/docs/leaf' },
-            { text: 'carrot backyard',link: '/en/docs/carrot' }
+            { text: 'Who are we?', link: '/en/docs/about' },
+            { text: 'Core members', link: '/en/docs/about/team'}
+        ]
+    },
+    {
+        text: 'SubRegions',
+        items: [
+            { text: "Kimi's Wonderland", link: '/en/docs/kimi' },
+            { text: 'Ghast Skylands', link: '/en/docs/kimi/skyland' },
+            { text: "Freshwater's Region", link: '/en/docs/water' },
+            { text: 'Leaf Page',link: '/en/docs/leaf' },
+            { text: 'WaterCarrot',link: '/en/docs/carrot' }
+        ]
+    },
+        {
+        text:'Rules',
+        items: [
+            { text: 'Content Edit Rule', link:'/en/docs/rule/EditRule'},
+            { text: 'Appropriate Messaging', link:'/en/docs/rule/MsgRule'},
+        ]
+    },
+    {
+        text: 'Services',
+        items:[
+            {text: 'Link Disk', link:'https://pan.link-et.link'},
+            {text: 'Online Maps', link:'https://map.link-et.link'},
         ]
     }
 ];
